eb2c0f6858b5324f8d5d7f615ac36dd4e3fbccf7
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-23 11:36:06 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PublicMapView from "./PublicMapView";
-import OfficeProfile from "./OfficeProfile";
-import Signup from "./Signup";
 import Login from "./Login";
+import OfficeDashboard from "./OfficeDashboard";
 
 const App = () => {
-  const [office, setOffice] = useState(null);
-
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={PublicMapView} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login">
-          <Login setOffice={setOffice} />
-        </Route>
-        <Route path="/office-profile">
-          <OfficeProfile office={office} setOffice={setOffice} />
-        </Route>
+        <Route path="/login" component={Login} />
+        <Route path="/office-dashboard" component={OfficeDashboard} />
+        {/* Add other routes as needed */}
       </Switch>
     </Router>
   );
