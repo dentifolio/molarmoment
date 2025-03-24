1dbc68a99ceaa025d6f38d86d747e21ae583678f
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-24 11:41:44 -0700

    Update AuthButtons.js

--- frontend/src/AuthButtons.js
+++ frontend/src/AuthButtons.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./AuthButtons.css"; // Add styles for the bubble buttons
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AuthButtons = () => {
   return (
-    <div className="auth-container">
-      <Link to="/login" className="auth-button">Login</Link>
-      <span className="separator">|</span>
-      <Link to="/signup" className="auth-button">Sign Up</Link>
+    <div>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Sign Up</Link>
     </div>
   );
 };
