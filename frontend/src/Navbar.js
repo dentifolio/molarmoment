2647f1daec1d7f7878e259147da8c646268a5a58
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-24 11:43:31 -0700

    Update Navbar.js

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,38 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AuthButtons from './AuthButtons';
 
-const AuthButtons = () => {
+const Navbar = () => {
   return (
-    <div className="auth-buttons">
-      <Link to="/login" className="auth-btn">
-        Login
-      </Link>
-      <Link to="/signup" className="auth-btn">
-        Sign Up
-      </Link>
-    </div>
-  );
-};
-
-const Navbar = ({ isLoggedIn, handleLogout }) => {
-  return (
-    <nav className="navbar">
-      <div className="nav-left">
-        <Link to="/" className="logo">
-          FindOpenDentist.com
-        </Link>
-      </div>
-      <div className="nav-right">
-        {isLoggedIn ? (
-          <button className="logout-btn" onClick={handleLogout}>
-            Logout
-          </button>
-        ) : (
-          // <AuthButtons /> // Removed login/signup buttons
-          null
-        )}
-      </div>
+    <nav>
+      <Link to="/">Home</Link>
+      <AuthButtons />
     </nav>
   );
 };
