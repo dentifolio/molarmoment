818fc734db40857d4f3615f58ab7ccf16005d39f
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-23 17:12:19 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import OfficeDashboard from "./OfficeDashboard";
-import PublicMapView from "./PublicMapView"; // Make sure to import PublicMapView
+import PublicMapView from "./PublicMapView";
+import Signup from "./Signup"; // Import Signup component
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<PublicMapView />} /> {/* Default route */}
         <Route path="/login" element={<Login />} />
         <Route path="/office-dashboard" element={<OfficeDashboard />} />
+        <Route path="/signup" element={<Signup />} /> {/* Signup route */}
         {/* Add other routes as needed */}
       </Routes>
     </Router>
