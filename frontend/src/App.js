21ad2bc6de36002faa2999ba3166ac9ba88fea68
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-25 00:28:04 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Login from './Login';
-import Signup from './Signup';
-import PublicMapView from './PublicMapView';
-import OfficeDashboard from './OfficeDashboard';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import OfficeAuth from './components/OfficeAuth';
+import OfficeDashboard from './components/OfficeDashboard';
+import PatientSearch from './components/PatientSearch';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<OfficeDashboard />} />
-          <Route path="/" element={<PublicMapView />} />
-        </Routes>
-      </div>
+      <nav className="p-4 bg-gray-200">
+        <Link to="/" className="m-2">Home</Link>
+        <Link to="/login" className="m-2">Office Login</Link>
+        <Link to="/signup" className="m-2">Office Signup</Link>
+        <Link to="/dashboard" className="m-2">Office Dashboard</Link>
+        <Link to="/search" className="m-2">Find a Dentist</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<OfficeAuth mode="login" />} />
+        <Route path="/signup" element={<OfficeAuth mode="signup" />} />
+        <Route path="/dashboard" element={<OfficeDashboard />} />
+        <Route path="/search" element={<PatientSearch />} />
+        <Route path="/" element={<PatientSearch />} />
+      </Routes>
     </Router>
   );
 }
