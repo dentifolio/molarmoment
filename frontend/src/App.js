ded936cd3c4c0bc32db4a0e639bd3236031dbd1e
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-26 20:47:48 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import OfficeAuth from './components/OfficeAuth';
-import OfficeDashboard from './components/OfficeDashboard';
-import PatientSearch from './components/PatientSearch';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import './App.css'; // Assuming you have some global styles
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<PatientSearch />} />
-          <Route path="/signup" element={<OfficeAuth mode="signup" />} />
-          <Route path="/login" element={<OfficeAuth mode="login" />} />
-          <Route path="/dashboard" element={<OfficeDashboard />} />
-          <Route path="/search" element={<PatientSearch />} />
-        </Routes>
-      </Layout>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        {/* Add other routes here if needed */}
+      </Switch>
     </Router>
   );
 }
