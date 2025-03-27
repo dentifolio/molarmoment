e2b82adf5f93850ee1d7f967ec3375d1df0b5321
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-03-26 21:09:21 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import './App.css'; // Assuming you have some global styles
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={MainPage} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
         {/* Add other routes here if needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 }
