64d484f0a4e34575fc99add38850139a4ea99228
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-04-08 22:08:47 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // or 'src' if you keep code in src
-  build: {
-    outDir: 'dist',
-  },
 })
