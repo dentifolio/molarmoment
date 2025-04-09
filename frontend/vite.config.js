fe7b82239dafec8d45d2308735d0a58cc4217746
dentifolio <54336867+dentifolio@users.noreply.github.com>
2025-04-08 20:14:57 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  root: './', // Ensure this points to the directory containing index.html
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // Output directory
+  },
 });
